6587eddb32ea991a8d5130cfcc78676c30b082b0
MarceloBr69 <marcelo.burgos200405@gmail.com>
2024-03-04 19:36:39 -0300

    hola

--- algoritmos_II.js
+++ algoritmos_II.js
@@ -62,6 +62,29 @@ for(var i=0; i<3; i++) {
    }
 }
 //Se imprime:
-//0, 0, 0, 1, 0, 2
+//nulo
 
 //Septimo ejercicio
+function looping(x,y) {
+    for(var i=0; i<x; i++) {
+       for(var j=0; j<x; j++) {        
+           console.log(i*j);
+       } 
+    }
+ }
+ z = looping(3,3);
+ console.log(z);
+ //Imprime 0,0,0,0,1,2,0,2,4 
+
+ //Octavo ejercicio
+ function looping(x,y) {
+    for(var i=0; i<x; i++) {
+       for(var j=0; j<y; j++) {        
+          console.log(i*j);
+       } 
+    }
+    return x*y;
+ }
+ z = looping(3,5);
+ console.log(z);
+ 
