e64b510ec523fb52d5ceac86a7e0d0068fbc7edf
MarceloBr69 <marcelo.burgos200405@gmail.com>
2024-03-04 20:28:48 -0300

    Terminado

--- algoritmos_II.js
+++ algoritmos_II.js
@@ -89,3 +89,68 @@ function looping(x,y) {
  console.log(z);
 //Se imprime: 
 //0,0,0,0,0,0,1,2,3,4,0,4,6,8
+
+
+
+
+//Parte 2
+
+//Primer ejercicio
+console.log('Parte2')
+function printUpTo(x) {
+    // your code here
+    for(let i = 0; i<1001; i++){
+        if(i % 2 === 0){
+            console.log(i)
+        }
+    }
+    if(x<0){
+        console.log('false')
+    }
+}
+printUpTo(1000); // debería imprimir todos los enteros de 1 to 1000
+y = printUpTo(-10); // debería imprimir false
+console.log(y); // debería imprimir false
+  
+
+
+//Segundo ejercicio
+console.log('Segundo ejercicio')
+function printSum(x) {
+    var sum = 0;
+    for(let i = 0; i<=x; i++){
+        if(i % 2 === 0){
+            console.log(i);
+            sum += i;
+        }
+    }
+    return sum
+}
+  y = printSum(255) // debería imprimir todos los enteros de 0 a 255 y que cada entero imprima la suma parcial.
+  console.log(y) // debería imprimir 32640
+  
+
+
+//Terer ejercicio
+console.log('tercer ejercicio')
+
+
+function printSumArray(x) {
+var sum = 0;
+    for(var i=0; i<x.length; i++) {
+        sum += x[i];
+    }
+    return sum;
+}
+console.log( printSumArray([1,2,3]) ); // debería imprimir 6
+
+
+//Bonus
+console.log('Bonus')
+
+function largestElement(arr){
+    let max = Math.max(...arr);
+    console.log(max);
+}
+
+largestElement([1,30,5,7]);
